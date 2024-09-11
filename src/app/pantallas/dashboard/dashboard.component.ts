9df073078146c16ab334daa2184c7bcce4a2ffc3
JuanfeCORHUILA <jf.garzon_2019-1@corhuila.edu.co>
2024-09-10 21:12:47 -0500

    feat funciones del dashboard principal

--- src/app/pantallas/dashboard/dashboard.component.ts
+++ src/app/pantallas/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import { DashboardService } from 'src/app/services/dashboard.service';
 
 Chart.register(...registerables);
 
@@ -8,21 +9,91 @@ Chart.register(...registerables);
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  egresadosNeiva: number = 0
+  egresadosPitalito: number = 0
+  ultimosEgresadosList: string[] = [];
+  
   @ViewChild('myChart') myChart!: ElementRef<HTMLCanvasElement>;
   chart!: Chart;
 
-  constructor(){}
+  constructor(
+    private dashboardService: DashboardService
+  ){}
+
+  ngOnInit(): void {
+    this.totalEstudiantesSede()
+    this.totalEstudiantesAño()
+    this.ultimosEgresados()
+  }
+
+  ultimosEgresados() {
+    this.dashboardService.ultimosEgresados().subscribe(
+      data => {
+        if (data.codResponse === 200) {
+          this.ultimosEgresadosList = data.listObject as string[];
+        } else {
+          console.error('Error al obtener los últimos egresados:', data.message);
+        }
+      },
+      error => {
+        console.error('Error al llamar el servicio de últimos egresados:', error);
+      }
+    );
+  }
+
+  totalEstudiantesSede(){
+
+    this.dashboardService.listEgresadosSede("Neiva").subscribe(
+      data => {
+        console.log(data);
+        this.egresadosNeiva = data.object
+      }
+    );
+
+    this.dashboardService.listEgresadosSede("Pitalito").subscribe(
+      data => {
+        console.log(data);
+        this.egresadosPitalito = data.object
+      }
+    );
+
+  }
+
+  totalEstudiantesAño() {
+    this.dashboardService.listEgresadosAño().subscribe(
+      data => {
+        if (data.codResponse === 200) {
+          const egresadosPorAño = data.listObject[0]; 
+          const años = Object.keys(egresadosPorAño);
+          const totales = Object.values(egresadosPorAño).map(value => Number(value));
+  
+          this.updateChart(años, totales);
+        } else {
+          console.error('No se pudieron obtener los egresados por año');
+        }
+      },
+      error => {
+        console.error('Error al obtener egresados por año:', error);
+      }
+    );
+  }
+
+  updateChart(labels: string[], data: number[]) {
+    this.chart.data.labels = labels;
+    this.chart.data.datasets[0].data = data;
+    this.chart.update();
+  }
 
   ngAfterViewInit() {
     this.chart = new Chart(this.myChart.nativeElement, {
       type: 'line',
       data: {
-        labels: ['2019', '2020', '2021', '2022', '2023', '2024', '2025'],
+        labels: [], 
         datasets: [{
           label: 'Egresados por período',
-          data: [20, 50, 25, 75, 55, 65, 85],
+          data: [],
           fill: false,
           borderColor: 'rgb(75, 192, 192)',
           tension: 0.1
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,5 +1,5 @@
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ThisReceiver } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, throwError } from 'rxjs';
@@ -13,23 +13,23 @@ import { Usuario, UsuarioResponse } from '../domain/listar';
 })
 export class DashboardService {
 
-  protected baseUrlApi: string;
-  protected baseUrlAuth: string;
-  protected  urlImage: string;
+  noticiasURL = environment.apiResURL + '/egresados/';
 
-  constructor(protected http: HttpClient) {
-    this.baseUrlApi = environment.urlApi;
-    this.baseUrlAuth = environment.urlauth;
-    this.urlImage = environment.urlImage;
-  }
+
+
+  constructor(protected httpClient: HttpClient) { }
   
-  /**
-   * Metodo para listar los usuarios en el deshboard
-   * @returns Observable<ListaUsuarios>
-   */
-  public listarUsuarios(): Observable<any> {
-    const url = this.baseUrlApi + '/egresados/listar';
-    return this.http.get(url);
+  public listEgresadosSede(sede: string): Observable<any> {
+    const params = new HttpParams().set('sede', sede)
+    return this.httpClient.get<any>(this.noticiasURL + 'totalEgresadosSede', { params })
+  }
+
+  public listEgresadosAño(): Observable<any> {
+    return this.httpClient.get<any>(this.noticiasURL + 'totalEgresadosAño')
+  }
+
+  public ultimosEgresados(): Observable<any> {
+    return this.httpClient.get<any>(this.noticiasURL + 'ultimosEgresados')
   }
   
 }
